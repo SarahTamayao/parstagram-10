c67095f0e9c3c3a77589b5c3872f9680d68d695d
Emily Jiang <emily.s.jiang@gmail.com>
2021-07-07 09:53:31 -0400

    Feed storyboard setup

--- Instagram/Views/PostCell.h
+++ Instagram/Views/PostCell.h
@@ -6,10 +6,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Post.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface PostCell : UITableViewCell
+@property (strong, nonatomic) Post *post;
+
+@property (weak, nonatomic) IBOutlet UIImageView *pfpView;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UIButton *likeButton;
+@property (weak, nonatomic) IBOutlet UIButton *commentButton;
+@property (weak, nonatomic) IBOutlet UIButton *sendButton;
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
+@property (weak, nonatomic) IBOutlet UILabel *likesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *captionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timestampLabel;
 
 @end
 
